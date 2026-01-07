78c21d3824bb3d58080f7aa1afb5ec892be34b3e
Putri <putritriyani180@gmail.com>
2026-01-07 18:54:35 +0700

    Update informasi

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ function kirimPesan() {
     document.getElementById("subjek").value = "";
     document.getElementById("pesan").value = "";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const coll = document.querySelectorAll(".collapsible");
+    coll.forEach(button => {
+        button.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
+
